41635332e60a6ec55246e21207831edd1f160b69
thiago.allue <thiago.allue@clearjet.com>
2025-02-11 19:18:59 -0300

    Refactor InputPasswordCustom to use styled-components for improved styling

--- frontend/components/InputPasswordCustom.jsx
+++ frontend/components/InputPasswordCustom.jsx
@@ -1,36 +1,36 @@
-import React from 'react';
-import { Input } from 'antd';
-import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
+import styled from 'styled-components'
+import { Input } from 'antd'
+import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons'
 
-export default function StyledPasswordInput({ value, onChange, style }) {
+const StyledPassword = styled(Input.Password)`
+  && {
+    height: 39px;
+    background-color: #fff9e6;
+    border: 1px solid #8B6A29;
+    border-radius: 6px;
+    box-sizing: border-box;
+
+    /* Placeholder styling */
+    ::placeholder {
+      color: #947038;
+      font-family: "Inter-Regular", Helvetica, sans-serif;
+      font-size: 12px;
+    }
+  }
+`
+
+export default function InputPasswordCustom({ value, onChange, style }) {
   return (
-    <>
-      <style jsx global>{`
-        ::placeholder {
-          color: #947038;
-          font-family: "Inter-Regular", Helvetica, sans-serif;
-          font-size: 12px;
-        }
-      `}</style>
-      <Input.Password
-        value={value}
-        onChange={onChange}
-        placeholder="Password"
-        style={{
-          height: '39px',
-          backgroundColor: '#fff9e6',
-          border: '1px solid #8B6A29',
-          borderRadius: '6px',
-          color: '#000000',
-          boxSizing: 'border-box',
-          ...style
-        }}
-        iconRender={(visible) =>
-          visible
-            ? <EyeTwoTone twoToneColor="#8B6A29" />
-            : <EyeInvisibleOutlined style={{ color: '#8B6A29' }} />
-        }
-      />
-    </>
-  );
+    <StyledPassword
+      value={value}
+      onChange={onChange}
+      placeholder="Password"
+      style={style}
+      iconRender={(visible) =>
+        visible
+          ? <EyeTwoTone twoToneColor="#8B6A29" />
+          : <EyeInvisibleOutlined style={{ color: '#8B6A29' }} />
+      }
+    />
+  )
 }
